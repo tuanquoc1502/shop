878add5535cf3cd6a7bb75cf43329c9a1c613175
nghia191 <mai.nam.nghia.bunbu@gmail.com>
2022-11-18 09:56:52 +0700

    edit Schema products

--- shop-back/app/models/Product.js
+++ shop-back/app/models/Product.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const Products = new Schema({
-    name: {type: String, require: true},
-    description: {type: String, require: true, maxLength: 600},
-    // image: {type: String, minLength: 5},
-}, {
+const Products = new Schema(
+  {
+    name: { type: String, require: true },
+    description: { type: String, require: true, maxLength: 600 },
+    price: { type: Number, require: true },
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-module.exports = mongoose.model('Products', Products)  
+module.exports = mongoose.model("Products", Products);
