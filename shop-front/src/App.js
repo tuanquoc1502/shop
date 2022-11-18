63dbdf9569462d66111ea62d74bd0a6555643dc5
nghia191 <mai.nam.nghia.bunbu@gmail.com>
2022-11-18 10:17:16 +0700

    test config auto deploy

--- shop-front/src/App.js
+++ shop-front/src/App.js
@@ -1,18 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 
 function App() {
-
   return (
     <div className="App">
-
+      <div>***</div>
 
       <Routes>
-      {routes.map((e, i) => (
-        <Route key={i} {...e} />
-      ))}
-      <Route path="*" element={<App />} />
-    </Routes>
+        {routes.map((e, i) => (
+          <Route key={i} {...e} />
+        ))}
+        <Route path="*" element={<App />} />
+      </Routes>
     </div>
   );
 }
