547b5ff481f6be35fb2b537a3eda685f9fbfedfe
nghia191 <mai.nam.nghia.bunbu@gmail.com>
2022-11-18 10:06:14 +0700

    config port

--- shop-back/index.js
+++ shop-back/index.js
@@ -1,17 +1,16 @@
 const path = require("path");
-const express = require('express')
-const app = express()
-const port = 5000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.use(cors())
-
+app.use(cors());
 
 const route = require("./routes");
-const db = require('./config/db')
+const db = require("./config/db");
 
 // Connect to DB
-db.connect()
+db.connect();
 
 app.use(express.json());
 
